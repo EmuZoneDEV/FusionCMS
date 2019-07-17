da95976a69b3fa89a010add23b212a3104b25b4c
EmuZoneDEV <52322805+EmuZoneDEV@users.noreply.github.com>
2019-07-17 03:43:29 +0200

    Update and rename del.me to TC_Rochet2AIO.cmd

---
+++ extras/docs/scripts/TC_Rochet2AIO.cmd
@@ -0,0 +1,5 @@
+@echo off
+Title TrinityCore Server Building - Cloning Source...
+Color 9e
+mode con lines=15 cols=65
+git clone https://github.com/Rochet2/AIO.git -b master --recursive .\Projects\TC_AIO
